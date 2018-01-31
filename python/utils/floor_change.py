a58414db8a768139cf40f337f6ab21005a00f013
Husky <husky@ttic.edu>
2018-01-31 10:41:35 -0600

    bugfix

--- python/utils/floor_change.py
+++ python/utils/floor_change.py
@@ -1,16 +1,17 @@
 #!/usr/bin/env python
 
 import time
+import sys
 
 from lcm import LCM
-from erlcm.floor_change_t import floor_change_t
+from erlcm.floor_change_msg_t import floor_change_msg_t
 
 
 
-def floor_change (floor_number):
+def publish_floor_change (floor_number):
     lcm = LCM()
 
-    msg = floor_change_t()
+    msg = floor_change_msg_t()
     msg.utime = int(time.time() * 1e6)
     msg.floor_no = floor_number
 
@@ -21,7 +22,7 @@ def floor_change (floor_number):
 
 
 if len(sys.argv) == 2:
-    floor = sys.argv[1]
+    floor = int(sys.argv[1])
     publish_floor_change (floor)
 else:
     print "hr-floor-change FloorNum"
